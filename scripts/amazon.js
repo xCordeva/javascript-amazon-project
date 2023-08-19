752c2147dad932e75bed877a333582867cb309bc
Cordeva <ahmed.abdelaaall@gmail.com>
2023-08-19 05:36:37 +0300

    add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,9 +45,29 @@ products.forEach((product)=>{
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-cart" data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>`
 })
 document.querySelector('.js-product-grid').innerHTML= productsHTML
+document.querySelectorAll('.js-add-cart').forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        const productId = button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId === item.productId){
+                matchingItem = item;
+            }
+        }) 
+        if (matchingItem){
+            matchingItem.quantity += 1
+        }else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            })
+        }
+        console.log(cart)
+    })
+})
